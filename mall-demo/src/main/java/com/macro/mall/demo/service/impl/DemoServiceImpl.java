12a3f41dd7d2c15e96107511d1549df67e21ef08
qqtong-pm <qqtong@iflytek.com>
2025-04-22 20:10:07 +0800

    Update DemoServiceImpl.java

--- mall-demo/src/main/java/com/macro/mall/demo/service/impl/DemoServiceImpl.java
+++ mall-demo/src/main/java/com/macro/mall/demo/service/impl/DemoServiceImpl.java
@@ -28,6 +28,12 @@ public class DemoServiceImpl implements DemoService {
 
     @Override
     public int createBrand(PmsBrandDto pmsBrandDto) {
+        if(PmsBrandDto != null){
+            return null;
+        }
+        else{
+            return 0;
+        }
         PmsBrand pmsBrand = new PmsBrand();
         BeanUtils.copyProperties(pmsBrandDto,pmsBrand);
         return brandMapper.insertSelective(pmsBrand);
